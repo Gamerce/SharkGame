96de70a17169bc1e4ad41b2f8913724a33b0e1ff
ChristianGamerce <chj@gamerce.net>
2023-04-20 17:38:47 +0200

    reduced lag cans, added tower, pillar, car toilet - Chj
    Reduced lag from hitting cans, added interactable water tower, pillar, car, toilet.

--- Assets/ProceduralMeshExploder/Script/MeshExploder.cs
+++ Assets/ProceduralMeshExploder/Script/MeshExploder.cs
@@ -10,11 +10,23 @@ namespace ProceduralMeshExploder
     {
         public MeshExploderConfig Config;
         public Cluster ClusterPrefab;
+		MaterialPropertyBlock matPropBlock;
+
 
         Renderer parentRenderer;
         bool animating;
+		bool useForce;
+		ConstantForce[] forces;
 
-        public void Explode()
+		public bool preWarm = true;
+		Cluster[] clustersArray = null;
+		private void Start() {
+			if(preWarm){
+				BreakMesh();
+			}
+		}
+
+		public void Explode()
         {
             gameObject.SetActive(true);
             if (!animating)
@@ -24,10 +36,13 @@ namespace ProceduralMeshExploder
             }
         }
 
-        IEnumerator Animate()
-        {
-            Mesh defaultMesh;
+		private void OnDestroy() {
+			//Debug.LogError("Destroyed here.");
+		}
 
+		public void BreakMesh(){
+            Mesh defaultMesh;
+			
             if (transform.parent)
             {
                 if (transform.parent.TryGetComponent(out SkinnedMeshRenderer skinnedMeshRenderer))
@@ -37,74 +52,75 @@ namespace ProceduralMeshExploder
                         parentRenderer = skinnedMeshRenderer;
                         defaultMesh = new Mesh();
                         skinnedMeshRenderer.BakeMesh(defaultMesh);
-                        parentRenderer.enabled = false;
+						//parentRenderer.enabled = false;
                     }
                     else
                     {
                         Debug.LogWarning(messageParentMeshNotFound);
-                        yield break;
+						return;
                     }
                 }
                 else if (transform.parent.TryGetComponent(out MeshRenderer meshRenderer))
                 {
                     parentRenderer = meshRenderer;
-
+			
                     if (transform.parent.TryGetComponent(out MeshFilter parentMeshFilter))
                     {
                         if (parentMeshFilter.sharedMesh)
                         {
                             defaultMesh = parentMeshFilter.mesh;
-                            meshRenderer.enabled = false;
+                            //meshRenderer.enabled = false;
                         }
                         else
                         {
                             Debug.LogWarning(messageParentMeshNotFound);
-                            yield break;
+							return;
                         }
                     }
                     else
                     {
                         Debug.LogWarning(messageParentMeshNotFound);
-                        yield break;
+						return;
                     }
                 }
                 else
                 {
                     Debug.LogWarning(messageParentRendererNotFound);
-                    yield break;
+					return;
                 }
             }
             else
             {
                 Debug.LogWarning(messageParentNotFound);
-                yield break;
+				return;
             }
-
+			
             transform.localPosition = Vector3.zero;
             transform.localScale = Vector3.one;
             transform.localEulerAngles = Vector3.zero;
-            transform.parent = null;
-
+			//Transform oldParent = transform.parent;
+            //transform.parent = null;
+			
             Vector3[] importedVertices = defaultMesh.vertices;
             int[] importedTriangles = defaultMesh.triangles;
-
+			
             Vector3[] flatVertices = new Vector3[importedTriangles.Length];
             int[] flatTriangles = new int[importedTriangles.Length];
-
+			
             for (int i = 0; i < importedTriangles.Length; i++)
             {
                 flatVertices[i] = importedVertices[importedTriangles[i]];
                 flatTriangles[i] = i;
             }
-
+			
             ////////////////////////// Start Animation
-
+			
             Triangle[] triangles = new Triangle[importedTriangles.Length / 3];
-
+			
             for (int vertexIndex = 0; vertexIndex < importedTriangles.Length; vertexIndex += 3)
             {
                 int triangleIndex = vertexIndex / 3;
-
+			
                 triangles[triangleIndex] = new Triangle()
                 {
                     Vertices = new Vector3[3] { 
@@ -114,39 +130,40 @@ namespace ProceduralMeshExploder
                     }
                 };
             }
-
+			
             Bounds importedBounds = defaultMesh.bounds;
-
+			
             float vertexRadius = Vector3.Distance(importedBounds.min, importedBounds.max);
-
+			
             //Debug.DrawLine(importedBounds.min, importedBounds.max, Color.red, 30.0f, false);
-
+			
             Vector3[] clusterPoints = new Vector3[Config.Clusters];
-
+			
             for (int i = 0; i < Config.Clusters; i++)
             {
                 clusterPoints[i] = vertexRadius * Random.insideUnitSphere;
                 //Debug.DrawLine(transform.position, transform.position + clusterPoints[i], UnityEngine.Color.red, Config.Lifetime, false);
             }
-
+			
             List<Triangle>[] triangleGroups = new List<Triangle>[Config.Clusters];
-
-            for (int i = 0; i < triangleGroups.Length; i++) triangleGroups[i] = new List<Triangle>();
-
+			
+            for (int i = 0; i < triangleGroups.Length; i++)
+				triangleGroups[i] = new List<Triangle>();
+			
             List<Triangle> nonSeperatedTriangles = new List<Triangle>(triangles);
             Vector3 point;
             Triangle triangle;
-
+			
             while (nonSeperatedTriangles.Count > 0)
             {
                 float minDistance = Mathf.Infinity;
                 int closestPoint = -1;
                 triangle = nonSeperatedTriangles.First();
-
+			
                 for (int i = 0; i < clusterPoints.Length; i++)
                 {
                     point = clusterPoints[i].normalized;
-
+			
                 
                     float distance = (point - triangle.Position).sqrMagnitude;
                 
@@ -156,71 +173,301 @@ namespace ProceduralMeshExploder
                         closestPoint = i;
                     }
                 }
-
+			
                 nonSeperatedTriangles.RemoveAt(0);
                 triangleGroups[closestPoint].Add(triangle);
             }
-
+			
             List<Cluster> clusters = new List<Cluster>();
-
+			
             for (int i = 0; i < Config.Clusters; i++)
             {
                 if (triangleGroups[i].Count == 0) continue;
                 clusters.Add(GetNewCluster(triangleGroups[i].ToArray(), clusterPoints[i].normalized));
             }
-
-            Cluster[] clustersArray = clusters.ToArray();
-
-            MaterialPropertyBlock matPropBlock = new MaterialPropertyBlock();
+			
+            clustersArray = clusters.ToArray();
+			
+            matPropBlock = new MaterialPropertyBlock();
             ClusterPrefab.MeshRenderer.GetPropertyBlock(matPropBlock);
-
-
-            ConstantForce[] forces = new ConstantForce[0];
-
-            bool useForce = Config.AdditionalForce != Vector3.zero;
-
+			
+			
+            forces = new ConstantForce[0];
+			
+            useForce = Config.AdditionalForce != Vector3.zero;
+			
             if (useForce)
             {
                 forces = new ConstantForce[clustersArray.Length];
-
+			
                 for (int i = 0; i < clustersArray.Length; i++)
                 {
                     forces[i] = clustersArray[i].gameObject.AddComponent<ConstantForce>();
                 }
             }
 
-            float alpha = 0.0f;
+			for(int index = 0; index < clustersArray.Length; index++){
+				clustersArray[index].transform.SetParent(transform, true);
+				clustersArray[index].GetComponent<Renderer>().enabled = false;
+			}
+			
+            //float alpha = 0.0f;
+		}
 
-            while (animating)
-            {
-                alpha = Mathf.Min(1.0f, alpha + Time.deltaTime / Config.Lifetime);
-
-                for (int i = 0; i < Config.ColorAnimations.Length; i++)
-                {
-                    matPropBlock.SetColor(Config.ColorAnimations[i].MaterialProperty, Config.ColorAnimations[i].ColorOverLife.Evaluate(alpha));
+        IEnumerator Animate()
+        {
+			if(preWarm){
+				
+                if (transform.parent.TryGetComponent(out SkinnedMeshRenderer skinnedMeshRenderer)){
+                    if (skinnedMeshRenderer.sharedMesh){
+						skinnedMeshRenderer.enabled = false;
+                    }
                 }
-
-                for (int i = 0; i < clustersArray.Length; i++)
-                {
-                    clustersArray[i].MeshRenderer.SetPropertyBlock(matPropBlock);
-                    if(useForce) forces[i].force = Config.ForceMultiplier.Evaluate(alpha) * Config.AdditionalForce;
+                else if (transform.parent.TryGetComponent(out MeshRenderer meshRenderer)){
+                    if (transform.parent.TryGetComponent(out MeshFilter parentMeshFilter)){
+                        if (parentMeshFilter.sharedMesh){
+                            meshRenderer.enabled = false;
+                        }
+                    }
                 }
+				for(int index = 0; index < clustersArray.Length; index++){
+					clustersArray[index].gameObject.SetActive(true);
+					clustersArray[index].transform.SetParent(null, true);
+					clustersArray[index].GetComponent<Renderer>().enabled = true;
+				}
+				float alpha = 0.0f;
+				
+				transform.parent = null;
+				MaterialPropertyBlock matPropBlock = new MaterialPropertyBlock();
+				ClusterPrefab.MeshRenderer.GetPropertyBlock(matPropBlock);
+				while (animating)
+				{
+					alpha = Mathf.Min(1.0f, alpha + Time.deltaTime / Config.Lifetime);
+
+					for (int i = 0; i < Config.ColorAnimations.Length; i++)
+					{
+						matPropBlock.SetColor(Config.ColorAnimations[i].MaterialProperty, Config.ColorAnimations[i].ColorOverLife.Evaluate(alpha));
+					}
+
+					for (int i = 0; i < clustersArray.Length; i++)
+					{
+						clustersArray[i].MeshRenderer.SetPropertyBlock(matPropBlock);
+						if(useForce)
+							forces[i].force = Config.ForceMultiplier.Evaluate(alpha) * Config.AdditionalForce;
+					}
             
-                yield return new WaitForEndOfFrame();
+					yield return new WaitForEndOfFrame();
 
-                if (alpha == 1.0f)
-                {
-                    animating = false;
-                }
-            }
+					if (alpha == 1.0f)
+					{
+						animating = false;
+					}
+				}
 
-            for (int i = 0; i < clustersArray.Length; i++)
-            {
-                Destroy(clustersArray[i].gameObject);
-                yield return new WaitForEndOfFrame();
-            }
+				for (int i = 0; i < clustersArray.Length; i++)
+				{
+					Destroy(clustersArray[i].gameObject);
+					yield return new WaitForEndOfFrame();
+				}
+				
+				Debug.LogError("Destroyed here.");
+				Destroy(gameObject);
+			}
+			else{
+				Mesh defaultMesh;
+				
+				if (transform.parent)
+				{
+				    if (transform.parent.TryGetComponent(out SkinnedMeshRenderer skinnedMeshRenderer))
+				    {
+				        if (skinnedMeshRenderer.sharedMesh)
+				        {
+				            parentRenderer = skinnedMeshRenderer;
+				            defaultMesh = new Mesh();
+				            skinnedMeshRenderer.BakeMesh(defaultMesh);
+				            parentRenderer.enabled = false;
+				        }
+				        else
+				        {
+				            Debug.LogWarning(messageParentMeshNotFound);
+				            yield break;
+				        }
+				    }
+				    else if (transform.parent.TryGetComponent(out MeshRenderer meshRenderer)){
+				        parentRenderer = meshRenderer;
+				
+				        if (transform.parent.TryGetComponent(out MeshFilter parentMeshFilter)){
+				            if (parentMeshFilter.sharedMesh){
+				                defaultMesh = parentMeshFilter.mesh;
+				                meshRenderer.enabled = false;
+				            }
+				            else{
+				                Debug.LogWarning(messageParentMeshNotFound);
+				                yield break;
+				            }
+				        }
+				        else{
+				            Debug.LogWarning(messageParentMeshNotFound);
+				            yield break;
+				        }
+				    }
+				    else
+				    {
+				        Debug.LogWarning(messageParentRendererNotFound);
+				        yield break;
+				    }
+				}
+				else
+				{
+				    Debug.LogWarning(messageParentNotFound);
+				    yield break;
+				}
+				
+				transform.localPosition = Vector3.zero;
+				transform.localScale = Vector3.one;
+				transform.localEulerAngles = Vector3.zero;
+				transform.parent = null;
+				
+				Vector3[] importedVertices = defaultMesh.vertices;
+				int[] importedTriangles = defaultMesh.triangles;
+				
+				Vector3[] flatVertices = new Vector3[importedTriangles.Length];
+				int[] flatTriangles = new int[importedTriangles.Length];
+				
+				for (int i = 0; i < importedTriangles.Length; i++)
+				{
+				    flatVertices[i] = importedVertices[importedTriangles[i]];
+				    flatTriangles[i] = i;
+				}
+				
+				////////////////////////// Start Animation
+				
+				Triangle[] triangles = new Triangle[importedTriangles.Length / 3];
+				
+				for (int vertexIndex = 0; vertexIndex < importedTriangles.Length; vertexIndex += 3)
+				{
+				    int triangleIndex = vertexIndex / 3;
+				
+				    triangles[triangleIndex] = new Triangle()
+				    {
+				        Vertices = new Vector3[3] { 
+				            flatVertices[flatTriangles[vertexIndex]],
+				            flatVertices[flatTriangles[vertexIndex + 1]],
+				            flatVertices[flatTriangles[vertexIndex + 2]] 
+				        }
+				    };
+				}
+				
+				Bounds importedBounds = defaultMesh.bounds;
+				
+				float vertexRadius = Vector3.Distance(importedBounds.min, importedBounds.max);
+				
+				//Debug.DrawLine(importedBounds.min, importedBounds.max, Color.red, 30.0f, false);
+				
+				Vector3[] clusterPoints = new Vector3[Config.Clusters];
+				
+				for (int i = 0; i < Config.Clusters; i++)
+				{
+				    clusterPoints[i] = vertexRadius * Random.insideUnitSphere;
+				    //Debug.DrawLine(transform.position, transform.position + clusterPoints[i], UnityEngine.Color.red, Config.Lifetime, false);
+				}
+				
+				List<Triangle>[] triangleGroups = new List<Triangle>[Config.Clusters];
+				
+				for (int i = 0; i < triangleGroups.Length; i++) triangleGroups[i] = new List<Triangle>();
+				
+				List<Triangle> nonSeperatedTriangles = new List<Triangle>(triangles);
+				Vector3 point;
+				Triangle triangle;
+				
+				while (nonSeperatedTriangles.Count > 0)
+				{
+				    float minDistance = Mathf.Infinity;
+				    int closestPoint = -1;
+				    triangle = nonSeperatedTriangles.First();
+				
+				    for (int i = 0; i < clusterPoints.Length; i++)
+				    {
+				        point = clusterPoints[i].normalized;
+				
+				    
+				        float distance = (point - triangle.Position).sqrMagnitude;
+				    
+				        if (distance < minDistance)
+				        {
+				            minDistance = distance;
+				            closestPoint = i;
+				        }
+				    }
+				
+				    nonSeperatedTriangles.RemoveAt(0);
+				    triangleGroups[closestPoint].Add(triangle);
+				}
+				
+				List<Cluster> clusters = new List<Cluster>();
+				
+				for (int i = 0; i < Config.Clusters; i++)
+				{
+				    if (triangleGroups[i].Count == 0) continue;
+				    clusters.Add(GetNewCluster(triangleGroups[i].ToArray(), clusterPoints[i].normalized));
+				}
+				
+				Cluster[] clustersArray = clusters.ToArray();
+				
+				MaterialPropertyBlock matPropBlock = new MaterialPropertyBlock();
+				ClusterPrefab.MeshRenderer.GetPropertyBlock(matPropBlock);
+				
+				
+				ConstantForce[] forces = new ConstantForce[0];
+				
+				bool useForce = Config.AdditionalForce != Vector3.zero;
+				
+				if (useForce)
+				{
+				    forces = new ConstantForce[clustersArray.Length];
+				
+				    for (int i = 0; i < clustersArray.Length; i++)
+				    {
+				        forces[i] = clustersArray[i].gameObject.AddComponent<ConstantForce>();
+				    }
+				}
+				
+				float alpha = 0.0f;
 
-            Destroy(gameObject);
+				while (animating)
+				{
+					alpha = Mathf.Min(1.0f, alpha + Time.deltaTime / Config.Lifetime);
+
+					for (int i = 0; i < Config.ColorAnimations.Length; i++)
+					{
+						matPropBlock.SetColor(Config.ColorAnimations[i].MaterialProperty, Config.ColorAnimations[i].ColorOverLife.Evaluate(alpha));
+					}
+
+					for (int i = 0; i < clustersArray.Length; i++)
+					{
+						clustersArray[i].MeshRenderer.SetPropertyBlock(matPropBlock);
+						if(useForce)
+							forces[i].force = Config.ForceMultiplier.Evaluate(alpha) * Config.AdditionalForce;
+					}
+            
+					yield return new WaitForEndOfFrame();
+
+					if (alpha == 1.0f)
+					{
+						animating = false;
+					}
+				}
+
+				for (int i = 0; i < clustersArray.Length; i++)
+				{
+					Destroy(clustersArray[i].gameObject);
+					yield return new WaitForEndOfFrame();
+				}
+				
+				Debug.LogError("Destroyed here.");
+				Destroy(gameObject);
+			}
+
         }
 
         public Cluster GetNewCluster(Triangle[] triangleGroup, Vector3 direction)
---
+++ Assets/Script/PillarFall.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarFall : MonoBehaviour
+{
+	public Vector3 tiltDirection;
+
+	Transform target;
+	Vector3 velocity = Vector3.zero;
+	public float fallSpeed = 5;
+	Vector3 tilt;
+
+	bool isFalling = false;
+	[Range(0,10)]
+	public float tiltStrength = 3;
+	public GameObject splatter;
+
+	public Transform testObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        angle = 0;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(isFalling)
+			UpdateFalling();
+		//TestFalling();
+    }
+	public float angle = 0;
+	public Vector3 forwardMod = new Vector3(90,0,0);
+	void TestFalling(){
+		tilt = testObj.position - transform.position;
+		tilt.y = 0;
+
+		Vector3 dir = tilt.normalized;
+		Vector3 axis = Vector3.Cross(-dir, Vector3.up);
+		float tiltAmount = tilt.magnitude;
+		angle = ((Mathf.Clamp(tiltAmount, 0, tiltStrength) / tiltStrength)) * 90;
+		if(angle > 80){
+			enabled = false;
+			Vector3 localEu = splatter.transform.localRotation.eulerAngles;
+			splatter.transform.parent = transform.parent;
+			splatter.transform.localRotation = Quaternion.Euler(localEu);
+			splatter.transform.position = target.position;
+			splatter.SetActive(true);
+		}
+		transform.rotation = Quaternion.AngleAxis(angle, axis);
+
+
+
+		//transform.LookAt(transform.position + tilt, Vector3.right);
+		////transform.localRotation = transform.localRotation * Quaternion.Euler(forwardMod);
+		//float tiltAmount = tilt.magnitude;
+		//transform.localRotation = Quaternion.Lerp(Quaternion.identity, transform.localRotation, (tiltAmount/tiltStrength).Clamp01());
+	}
+
+	void UpdateFalling(){
+		tilt += velocity * Time.deltaTime;
+		//if(Vector3.Dot(tilt.normalized, transform.position - target.position) < 0){
+		//	velocity -= (transform.position - target.position).normalized * Time.deltaTime;
+		//}
+		//else
+		//	velocity += (transform.position - target.position).normalized * Time.deltaTime;
+		velocity += (target.position - transform.position).normalized * Time.deltaTime * fallSpeed;
+		velocity = Vector3.Lerp(velocity, Vector3.zero, Time.deltaTime);
+		
+		Vector3 dir = tilt.normalized;
+		Vector3 axis = Vector3.Cross(-dir, Vector3.up);
+		float tiltAmount = tilt.magnitude;
+		angle = ((Mathf.Clamp(tiltAmount, 0, tiltStrength) / tiltStrength)) * 90;
+		if(angle > 70){
+			enabled = false;
+			Vector3 localEu = splatter.transform.localRotation.eulerAngles;
+			splatter.transform.parent = transform.parent;
+			splatter.transform.localRotation = Quaternion.Euler(localEu);
+			splatter.transform.position = target.position;
+			splatter.SetActive(true);
+		}
+		transform.rotation = Quaternion.AngleAxis(angle, axis);
+
+		//float tiltAmount = tilt.magnitude;
+		//transform.LookAt(transform.position + tilt);
+		//transform.localRotation = Quaternion.Lerp(Quaternion.identity, transform.localRotation, (tiltAmount/tiltStrength).Clamp01());
+	}
+
+	public void TriggerFall(Transform hitTarget, float hitSize = 0.5f){
+		target = hitTarget;
+		velocity = forwardMod;
+		isFalling = true;
+	}
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.color = Color.black;
+		Gizmos.DrawLine(transform.position + Vector3.up*1.5f, transform.position + Vector3.up*1.5f + tiltDirection);
+	}
+}
